ead834370c8bcbec5c8d0bb7bcf110993f786d90
Bingo <faaa@live.com>
2022-06-21 17:13:14 +0800

    第5节课 看完

---
+++ 10_组件化/ComponentA.vue
@@ -0,0 +1,27 @@
+<template>
+ <h2> {{ title }}</h2>
+ <p> {{ desc }}</p>
+ <button @click="btnClick"></button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "我是标题",
+      desc: "我是内容"
+    }
+  },
+  methods: {
+    btnClick() {
+      console.log("button clicked")
+    }
+  }
+}
+
+</script>
+
+<style>
+
+
+</style>
